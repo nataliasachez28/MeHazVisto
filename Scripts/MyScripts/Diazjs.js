992f503b4377993833be6d85b0f03cc76986335c
nataliasachez28 <nadia.sanchez28@gmail.com>
2014-11-27 10:37:03 -0600

    Revert "Me has visto"
    This reverts commit 13bea8091feec1a6fd366ad844b9a8876672c309.

--- Scripts/MyScripts/Diazjs.js
+++
@@ -1,7 +0,0 @@
-﻿
-function Acceso() {
-    while (prompt("Ingrese contraseña") != "tecnologicoluzdelaciencia") {
-        alert("Acceso denegado");
-    }
-    alert("Gracias disfruta el programa");
-}
